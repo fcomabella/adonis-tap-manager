1ad4c56bf4cebda58b96fe5ea83cde7884992364
Ferran Comabella <fcomabella@users.noreply.github.com>
2017-01-27 20:26:53 +0100

    Travis CI setup

--- src/test/functional/country.spec.js
+++ src/test/functional/country.spec.js
@@ -1,11 +1,10 @@
 const chai = use('chai')
+const should = chai.should()
 const request = use('supertest')
-const assert = chai.assert
 const co = use('co')
 const Ioc = use('adonis-fold').Ioc
 const baseUrl = `http://${process.env.HOST}:${process.env.PORT}/`
 const apiUrl = `${baseUrl}api/v1/`
-const should = require('should')
 use('co-mocha')
 
 describe('Country controller', function () {
@@ -26,7 +25,7 @@ describe('Country controller', function () {
             .expect(200)
             .expect('Content-Type', /json/)
 
-        response.body.should.be.an.Array()
+        response.body.should.be.an.Array
         response.body.length.should.equal(0)
     })
 
@@ -40,7 +39,7 @@ describe('Country controller', function () {
             .expect(200)
             .expect('Content-Type', /json/)
 
-        response.body.should.be.an.Array()
+        response.body.should.be.an.Array
         response.body.length.should.equal(1)
         response.body[0].name.should.match(/Catalunya/)
     })
@@ -55,7 +54,7 @@ describe('Country controller', function () {
             .expect(200)
             .expect('Content-Type', /json/)
 
-        response.body.should.be.Object()
+        response.body.should.be.Object
         response.body.name.should.match(/Catalunya/)
     })
 
@@ -64,7 +63,7 @@ describe('Country controller', function () {
             .get('countries/0')
             .expect(404)
 
-        response.body.should.be.Object()
+        response.body.should.be.Object
     })
 
     it('should return 201 for a new non existing country', function* () {
@@ -76,8 +75,8 @@ describe('Country controller', function () {
             .expect(201)
             .expect('Content-Type', /json/)
 
-        response.body.should.be.Object()
-        response.body.name.should.be.a.String()
+        response.body.should.be.Object
+        response.body.name.should.be.a.string
     })
 
     it('should return 400 for a new country without name', function* () {
@@ -89,7 +88,7 @@ describe('Country controller', function () {
             .expect(400)
             .expect('Content-Type', /json/)
 
-        response.body.should.be.Object()
+        response.body.should.be.Object
     })
     it('should return 409 for a new country with an existing name', function* () {
         const catalunya = yield use('App/Model/Country').create({
@@ -104,7 +103,7 @@ describe('Country controller', function () {
             .expect(409)
             .expect('Content-Type', /json/)
 
-        response.body.should.be.Object()
+        response.body.should.be.Object
     })
     it('should return 404 when updating a non existing country id', function* () {
         const response = yield request(apiUrl)
@@ -114,7 +113,7 @@ describe('Country controller', function () {
             })
             .expect(404)
 
-        response.body.should.be.Object()
+        response.body.should.be.Object
     })
     it('should return 200 and update a changed country if the new name doesn\'t exist', function* () {
         const catalunya = yield use('App/Model/Country').create({
@@ -129,7 +128,7 @@ describe('Country controller', function () {
             .expect(200)
             .expect('Content-Type', /json/)
 
-        response.body.should.be.Object()
+        response.body.should.be.Object
     })
 
     it('should return 400 for an update to no name', function* () {
@@ -145,7 +144,7 @@ describe('Country controller', function () {
             .expect(400)
             .expect('Content-Type', /json/)
 
-        response.body.should.be.Object()
+        response.body.should.be.Object
     })
 
     it('should return 409 for an update to a previously existing name', function* () {
@@ -165,7 +164,7 @@ describe('Country controller', function () {
             .expect(409)
             .expect('Content-Type', /json/)
 
-        response.body.should.be.Object()
+        response.body.should.be.Object
     })
 
     it('should return 204 for a successful delete', function* () {
@@ -177,7 +176,7 @@ describe('Country controller', function () {
             .delete(`countries/${catalunya.id}`)
             .expect(204)
 
-        response.body.should.be.empty()
+        response.body.should.be.empty
     })
 
     it('should return 404 for a deletion to a non existing id',function* () {
@@ -185,6 +184,6 @@ describe('Country controller', function () {
             .delete('countries/0')
             .expect(404)
 
-        response.body.should.be.Object()
+        response.body.should.be.Object
     })
 })
